527ca848c209ca6aff60c243a10a0ad4b4ad053a
Mahesh8188 <ddbambhava8182@gmail.com>
2025-09-14 08:12:26 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -12,7 +12,7 @@ logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
 class Config(object):
     # Get a bot token from @botfather
-    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
+    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "8428021322:AAECwD0MngeFR0lkdVf3kUzDIz8Vvr2ynKE")
 
     # Get from my.telegram.org
     APP_ID = int(os.environ.get("APP_ID", "26741021"))
